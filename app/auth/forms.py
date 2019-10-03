9ba36f56b0ac27042908bc54c0cb3b9c2028b3cb
Nasaruddin <nasaruddin@advpubtech.com>
2019-10-03 16:52:22 +0700

    add login, register, confirm, logout

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -11,3 +11,31 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
 
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField(
+        'Username',
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                '^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                'Usernames must have only letters, numbers, dots or underscores'
+            )
+        ])
+    password = PasswordField('Password', validators=[
+        DataRequired(), EqualTo('password2', message="Passwords must match.")
+    ])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already reqistered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
